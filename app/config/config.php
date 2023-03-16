a4e5be0f85b3d8849a08baa18414a1f095ea6f0f
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-03-16 14:10:16 +0100

    config

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("URLROOT", "http://mvc-oop-toets.com");
+define("APPROOT", dirname(dirname(__FILE__)));
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc-oop-toets');
