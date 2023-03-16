5f85f4e94a90e5dcd5b0ddb79682e25999e56859
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2023-03-16 14:13:25 +0100

    MVC framework

---
+++ app/controllers/Voetballer.php
@@ -0,0 +1,34 @@
+<?php
+
+class Voetballer extends BaseController
+{
+    private $voetbalModel;
+
+    public function __construct()
+    {
+        $this->voetbalModel = $this->model('VoetballerModel');
+    }
+
+    public function index()
+    {
+        $result = $this->voetbalModel->getVoetballers();
+
+        $rows = "";
+        foreach ($result as $voetballer) {
+            $rows .= "<tr>
+                        <td>$voetballer->Naam</td>
+                        <td>$voetballer->Club</td>
+                        <td>$voetballer->Leeftijd</td>
+                        <td>$voetballer->Nationaliteit</td>
+                        <td>$voetballer->Salaris</td>
+                      </tr>";
+        }
+
+        $data = [
+            'title' => 'Top 5 best betaalde voetballers ter wereld',
+            'rows' => $rows
+        ];
+
+        $this->view('voetballer/voetballer', $data);
+    }
+}
---
+++ app/views/country/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <title>Country</title>
+</head>
+<body>
+
+<header>
+        <a href="<?= URLROOT; ?>" class="logo">
+            <i class="ri-home-3-fill"></i>
+            <span>TJARDO</span>
+        </a>
+
+        <ul class="navbar">
+            <li><a href="<?= URLROOT; ?>">Home</a></li>
+            <li><a href="<?= URLROOT; ?>/country" class="active">Country</a></li>
+            <li><a href="<?= URLROOT; ?>/voetballer">Voetballers</a></li>
+            <li><a href="<?= URLROOT; ?>/vulkaan">Vulkanen</a></li>
+        </ul>
+
+        <div class="main">
+            <a href="#" class="user">
+                <i class="ri-user-fill"></i>
+                Sign In
+            </a>
+            <a href="#">Register</a>
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div>
+    </header>
+
+    <h3><?= $data['title']; ?></h3>
+
+    <table border='1'>
+        <thead>
+            <th>Id</th>
+            <th>Naam</th>
+        </thead>
+        <tbody>
+            <?= $data['rows']; ?>
+        </tbody>
+    </table>
+
+    <script src="<?= URLROOT; ?>/js/nav.js"></script>
+
+</body>
+</html>
